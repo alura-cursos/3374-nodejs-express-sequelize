580e190f557f86aeebe112c85c9dae0466fb9802
JulianaAmoasei <late.parrot@gmail.com>
2023-09-05 15:55:47 -0300

    video: conectando model e controller

--- src/controllers/PessoaController.js
+++ src/controllers/PessoaController.js
@@ -1,7 +1,10 @@
+const database = require('../models');
+
 class PessoaController {
   static async pegaTodas (req, res) {
     try {
-      // acessar o modelo
+      const listaDePessoas = await database.Pessoa.findAll();
+      return res.status(200).json(listaDePessoas);
     } catch (erro) {
       // erro
     }
