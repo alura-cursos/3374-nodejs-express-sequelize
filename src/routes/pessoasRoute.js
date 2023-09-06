f98660ad389d06095277cc7fda4f1e58e0cceab5
JulianaAmoasei <late.parrot@gmail.com>
2023-09-06 12:37:17 -0300

    video: finalizando o CRUD

--- src/routes/pessoasRoute.js
+++ src/routes/pessoasRoute.js
@@ -6,5 +6,9 @@ const pessoaController = new PessoaController();
 const router = Router();
 
 router.get('/pessoas', (req, res) => pessoaController.pegaTodos(req, res));
+// router.get('/pessoas/:id', (req, res) => pessoaController.pegaUmPorId(req, res));
+// router.post('/pessoas', (req, res) => pessoaController.criaNovo(req, res));
+router.put('/pessoas/:id', (req, res) => pessoaController.atualiza(req, res));
+// router.delete('/pessoas/:id', (req, res) => pessoaController.exclui(req, res));
 
 module.exports = router;
--- src/services/Services.js
+++ src/services/Services.js
@@ -8,6 +8,28 @@ class Services {
   async pegaTodosOsRegistros () {
     return dataSource[this.model].findAll();
   }
+
+  // async pegaUmRegistroPorId(id) {
+  //   return dataSource[this.model].findByPk(id);
+  // }
+
+  // async criaRegistro(dadosDoRegistro) {
+  //   return dataSource[this.model].create(dadosDoRegistro);
+  // }
+
+  async atualizaRegistro(dadosAtualizados, id) {
+    const listadeRegistrosAtualizados = dataSource[this.model].update(dadosAtualizados, {
+      where: { id: id }
+    });
+    if (listadeRegistrosAtualizados[0] === 0) {
+      return false;
+    }
+    return true;
+  }
+
+  // async excluiRegistro(id) {
+  //   return dataSource[this.model].destroy({ where: { id: id } });
+  // }
 }
 
 module.exports = Services;
