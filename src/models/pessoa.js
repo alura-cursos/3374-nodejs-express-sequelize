de8051c0ba8d57dbd59024a750ece35f8bb06acd
Neto Flavio <netoflavio97@hotmail.com>
2023-12-10 10:42:26 -0300

    feat/instalando e criando a primeira tabela do banco

---
+++ src/models/pessoa.js
@@ -0,0 +1,29 @@
+'use strict';
+const {
+  Model
+} = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Pessoa extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Pessoa.init({
+    nome: DataTypes.STRING,
+    email: DataTypes.STRING,
+    cpf: DataTypes.STRING,
+    ativo: DataTypes.BOOLEAN,
+    role: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: "Pessoa",
+    // deixa claro para o sqlite o nome da tabela, evita bugs.
+    tableName: "pessoas"
+  });
+  return Pessoa;
+};
