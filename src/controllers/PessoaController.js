6d0b159f986ed5750f7ecfc8a0f9a4bd1a4bd1e5
Renan de Oliveira <renan_oliveira93@outlook.com.br>
2024-01-19 17:07:01 -0300

    feat: create controlers

---
+++ src/controllers/PessoaController.js
@@ -0,0 +1,12 @@
+const Controller = require('./Controller.js');
+const PessoaServices = require('../services/PessoaServices.js');
+
+const pessoaServices = new PessoaServices();
+
+class PessoaController extends Controller {
+  constructor() {
+    super(pessoaServices);
+  }
+}
+
+module.exports = PessoaController;
