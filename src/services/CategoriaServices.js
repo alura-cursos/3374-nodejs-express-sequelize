02a45e971e88cf05c6431d08188d0e9582db1038
Renan de Oliveira <renan_oliveira93@outlook.com.br>
2024-01-19 17:07:39 -0300

    feat: create services

---
+++ src/services/CategoriaServices.js
@@ -0,0 +1,9 @@
+const Services = require('./Services.js');
+
+class CategoriaServices extends Services {
+  constructor() {
+    super('Categoria');
+  }
+}
+
+module.exports = CategoriaServices;
---
+++ src/services/Services.js
@@ -0,0 +1,35 @@
+const dataSource = require('../models');
+
+class Services {
+  constructor(nomeDoModel) {
+    this.model = nomeDoModel;
+  }
+
+  async obtemTodosOsRegistros() {
+    return dataSource[this.model].findAll();
+  }
+
+  async pegaUmRegistroPorId(id) {
+    return dataSource[this.model].findByPk(id);
+  }
+
+  async criaRegistro(dadosDoRegistro) {
+    return dataSource[this.model].create(dadosDoRegistro);
+  }
+
+  async atualizaRegistro(dadosAtualizados, id) {
+    const listadeRegistrosAtualizados = dataSource[this.model].update(dadosAtualizados, {
+      where: { id: id }
+    });
+    if (listadeRegistrosAtualizados[0] === 0) {
+      return false;
+    }
+    return true;
+  }
+
+  async excluiRegistro(id) {
+    return dataSource[this.model].destroy({ where: { id: id } });
+  }
+}
+
+module.exports = Services;
